5b650790bd9336d0b28e3c744197c20c10a556fe
Germán Eduardo Baltazar Reyes <germanebr@hotmail.com>
2022-02-02 00:40:41 -0600

    Added client and seller account methods into the navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_client
+  before_action :set_current_seller
+
+  def set_current_client
+    if session[:client_id]
+      Current.client = Client.find_by(id: session[:client_id])
+    end
+  end
+
+  def set_current_seller
+    if session[:seller_id]
+      Current.seller = Seller.find_by(id: session[:seller_id])
+    end
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,4 @@
 class MainController < ApplicationController
   def index
-    if session[:client_id]
-      @client = Client.find_by(id: session[:client_id])
-    elsif session[:seller_id]
-      @seller = Seller.find_by(id: session[:seller_id])      
-    end
   end
 end
